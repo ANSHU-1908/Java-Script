62cc82bca8d97260008769e21269c7fbd5eda9cf
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-07-27 01:01:26 +0000

    01_basics

---
+++ 01_basics/conversionops.js
@@ -0,0 +1,79 @@
+// let score  = '33'
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// let score  = '33abd'
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// let score  = 'Anshu'
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// let score  = null
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// let score  = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// let score  = true
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valInNumber = Number(score)
+
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
+
+// console.log(typeof NaN);   //  number  
+
+// let isLoggedIn = 55
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console. log(booleanIsLoggedIn);
+
+// 1 --> true , 0 --> false 
+// " " or ' '  --> false 
+// "ANshu"  --> true 
+// any number --> true 
+
+let someNumber = 55 
+
+let ValueInString = String(someNumber) 
+console.log(ValueInString);
+console.log(typeof ValueInString);
+
+
