41ab6ac95ff2a81af65a69c73ebafedabb332870
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-07-28 06:59:05 +0000

    conversionops

--- 01_basics/conversionops.js
+++ 01_basics/conversionops.js
@@ -1,79 +1,119 @@
-let score  = '33'
+// let score  = '33'
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-let score  = '33abd'
+// let score  = '33abd'
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-let score  = 'Anshu'
+// let score  = 'Anshu'
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-let score  = null
+// let score  = null
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-let score  = undefined
+// let score  = undefined
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-let score  = true
+// let score  = true
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valInNumber = Number(score)
+// let valInNumber = Number(score)
 
-console.log(typeof valInNumber )
-console.log(valInNumber)
+// console.log(typeof valInNumber )
+// console.log(valInNumber)
 
-console.log(typeof NaN);   //  number  
+// console.log(typeof NaN);   //  number  
 
-let isLoggedIn = 55
+// let isLoggedIn = 55
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console. log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console. log(booleanIsLoggedIn);
 
-1 --> true , 0 --> false 
-" " or ' '  --> false 
-"ANshu"  --> true 
-any number --> true 
+// 1 --> true , 0 --> false 
+// " " or ' '  --> false 
+// "ANshu"  --> true 
+// any number --> true 
 
-let someNumber = 55 
+// let someNumber = 55 
 
-let ValueInString = String(someNumber) 
-console.log(ValueInString);
-console.log(typeof ValueInString);
+// let ValueInString = String(someNumber) 
+// console.log(ValueInString);
+// console.log(typeof ValueInString);
+
+    //  operations  
+
+    // let value = "12" ;
+    // let negvalue = -Number(value) 
+    // console.log(negvalue);
+    //  console.log(typeof negvalue);
+    
+
+    //  console.log(2+2);
+    //  console.log(2-2);
+    //  console.log(2*2);
+    //  console.log(2/2);
+    //  console.log(2**3);      // ** means power :-->  this line means 2 to the power 3 
+    //  console.log(2%6); 
+
+    // let str1 = "Anhsu" ;
+    // let astr2  = "Mishra" ;
+
+    // let name = str1 +" " + astr2 ;
+
+    // console.log(name);
+
+//     console.log(+true);
+//     console.log(true); 
+
+//     let speed = 100 ;
+//     speed++ 
+
+//    console.log(speed); 
+   
+//    let inc  = ++speed  ;
+//    console.log(inc);
+
+   
+ //  === is used for strictly use to check data types and value too . 
+ 
+ console.log("2" === 2 );
+
+    
 
 
