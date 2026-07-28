61a123b89fa34ab3e0a80764b20a3c99f4b8fa61
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-07-28 06:58:51 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,29 @@
+// console.log(Number (2 > 2) );
+
+// console.log(typeof (Number (2 > 2) ));
+
+//  console.log(2 >= 2 );
+
+//  console.log(2 == 2 );
+
+//  console.log(2 <= 2 );
+
+//  console.log(2 < 2 );
+
+// console.log(2 != 2 );
+
+//  console.log(2 > "1" );
+
+//  console.log("02" > 1);  // make sure when we are comparing the data types should remain same 
+
+
+// console.log(null > 0); 
+//  console.log(null != 0);
+// console.log(null >= 0);
+
+
+
+// const bigNumnber = 3458794561324568974n ;
+
+console.log(typeof bigNumnber);
+
