558ff1ec37a7e9e649fbd7b0248cdeaa1bc5cb2c
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-07-30 06:56:00 +0000

    math&num

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,47 @@
+// const score = 400
+
+// console.log(score) 
+
+// const number = new Number(400) 
+
+// console.log(number);
+// let v = number.toString()
+
+// console.log(v); 
+// console.log(typeof v);
+
+// const othernumber = 234.65356 ;
+// console.log(othernumber.toFixed(2));    // give the value after decimal which mentioned 
+
+// console.log(othernumber.toPrecision(2));
+// console.log(othernumber.toPrecision(3));
+// console.log(othernumber.toPrecision(4));   // give exact value what has been mentioned  
+
+// const hundreds  = 1000000  
+
+// console.log(hundreds.toLocaleString());   // give the value to american standard
+// console.log(hundreds.toLocaleString('en-IN'));  // give the value to indian standard
+
+
+// ++++++++++++++ maths +++++++++
+
+//  console.log(Math) 
+//  console.log(Math.abs(-1)) 
+//  console.log(Math.round(4.5))
+//  console.log(Math.ceil(4.1))
+//  console.log(Math.floor(4.9))
+
+ console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
