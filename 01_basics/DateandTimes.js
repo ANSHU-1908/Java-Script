268e1352b13721ad0120102673be88aad10a8295
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-08-05 19:28:05 +0000

    Dates and TImes

---
+++ 01_basics/DateandTimes.js
@@ -0,0 +1,10 @@
+// let myDate  = new Date()
+
+// console.log(myDate);
+let mydate = new Date();  // give the current date and time
+console.log(mydate);
+console.log(typeof mydate);  // object
+console.log(mydate.toString());  // convert to string
+console.log(mydate.toLocaleString);
+
+
