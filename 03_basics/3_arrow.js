91992b5dee05be002106869880509c1de743f366
Diptanshu Kumar Mishra <166811096+ANSHU-1908@users.noreply.github.com>
2026-09-01 10:00:05 +0000

    arrow

---
+++ 03_basics/3_arrow.js
@@ -0,0 +1,72 @@
+// this keyword refers current context (values or talk about what)
+
+const user = {
+
+    username : "Anshu" ,
+    price : 499 ,
+    welcomemessage : function(){
+
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+        
+
+    }
+
+
+}
+
+// user.username = "harsh"
+// user.welcomemessage()
+// user.username = "Natta" 
+// user.welcomemessage() 
+
+
+// browser ke andar stand alonne jo object hai wo windows hai 
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     // console.log(this.username);
+//     console.log(this);
+    
+// }
+
+// const chai =  () => {
+
+    // let username = "hitesh"
+    // console.log(this.username);
+//     console.log(this);
+    
+// }
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
+
+
